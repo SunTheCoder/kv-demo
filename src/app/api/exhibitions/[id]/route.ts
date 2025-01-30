bed8391a6a74039aed39a9422ea5126bc2816d13
Sun English <sunenglishjr@gmail.com>
2025-01-30 12:17:43 -0500

    fix route

--- src/app/api/exhibitions/[id]/route.ts
+++ src/app/api/exhibitions/[id]/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { kv } from '@vercel/kv'
 import { Exhibition } from '../route'
 
 export async function PATCH(
-  request: Request,
-  context: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = context.params
+    const { id } = params
     const updates = await request.json()
     
     const exhibition = await kv.hgetall(`exhibition:${id}`) as Record<string, unknown>
